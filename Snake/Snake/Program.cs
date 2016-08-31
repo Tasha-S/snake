178f0ea7ad18c573d0d0af5eca8747912e0e72ec
Tasha-S <murrr-meow@bk.ru>
2016-08-31 19:45:00 +0300

    работа со списками

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -11,11 +11,29 @@ namespace Snake
         static void Main(string[] args)
         {
 
-            Point p1 = new Point(1, 3, '*');
-            p1.Draw();
+            List<char> chList = new List<char>();
+            chList.Add('*');
+            chList.Add('#');
+            chList.Add('@');
+            chList.Add('%');
 
-            Point p2 = new Point(4, 6, '#');
-            p2.Draw();
+
+            Point p1 = new Point(1, 3, chList[0]);
+            Point p2 = new Point(4, 6, chList[1]);
+            Point p3 = new Point(8, 10, chList[2]);
+            Point p4 = new Point(7, 6, chList[3]);
+           
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+            pList.Add(p3);
+            pList.Add(p4);
+
+            foreach (Point i in pList)
+            {
+                i.Draw();
+            }
 
             Console.ReadLine(); 
         }
